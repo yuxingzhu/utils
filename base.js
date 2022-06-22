3fe90f7ddab9fa9de944c588b9cf6116842a6930
YX <zyx19941019@163.com>
2022-06-22 20:59:07 +0800

    Update base.js

--- base.js
+++ base.js
@@ -1,3 +1,4 @@
+import { MessageBox, Message } from 'yuwp-ui';
 import { isNil, isNaN } from 'lodash';
 
 // 获取当前时间
@@ -69,4 +70,51 @@ export function toPoint(percent) {
   let str = percent.replace('%', '').replace(',', '');
   str /= 100;
   return str;
+}
+
+// 显示信息
+export function showMessage(msg, type = 'error', duration = 5) {
+  msg && Message({
+    message: msg,
+    type: type,
+    duration: duration * 1000
+  });
+}
+
+// 显示信息
+export function showMessageAlert(msg, type= 'error') {
+  msg && MessageBox({
+    message: msg,
+    type: type
+  });
+}
+
+// 判断浏览器类型
+export function getBrowserType() {
+  // 取得浏览器的userAgent字符串
+  var userAgent = navigator.userAgent; 
+  // 判断是否Opera浏览器 OPR/43.0.2442.991
+  if(userAgent.indexOf('OPR') > -1) {
+    return 'Opera';
+  } 
+  // 判断是否Firefox浏览器 Firefox/51.0
+  if(userAgent.indexOf('FireFox') > -1) {
+    return 'FF';
+  }
+  // 判断是否IE浏览器 Trident/7.0;
+  if(userAgent.indexOf('Trident') > -1) {
+    return 'IE';
+  }
+  // 判断是否Edge浏览器 Edeg/14.14393
+  if(userAgent.indexOf('Edge') > -1) {
+    return 'Edge';
+  }
+  // 判断是否Chrome浏览器 Chrome/56.0.2924.87
+  if(userAgent.indexOf('Chrome') > -1) {
+    return 'Chrome';
+  }
+   // 判断是否Safari浏览器 AppleWebKit/534.57.2
+  if(userAgent.indexOf('Safari') > -1) {
+    return 'Safari';
+  }
 }
