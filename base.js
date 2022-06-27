96e030d2e5e92178fbba4fbce645e51814b28ceb
YX <zyx19941019@163.com>
2022-06-27 16:21:01 +0800

    Update base.js

--- base.js
+++ base.js
@@ -142,6 +142,81 @@ export function moneyToUpper(money) {
   // 整型完以后的单位
   var cnIntLast = '元';
   
+  // 最大处理的数字
+  var maxNum = 999999999999999.9999;
   
+  // 金额整数部分
+  var integerNum;
   
+  // 金融小数部分
+  var decimalNum;
+  
+  // 输出的中文金额字符串
+  var chineseStr = '';
+  
+  // 分离金额后用的数组，预定义
+  var parts;
+  if (money === '') {
+    return '';
+  } 
+  money = parseFloat(money);
+  if (money >= maxNum) {
+    // 超出最大处理数字
+    return '';
+  }
+  if (money === 0) {
+    chineseStr = cnNums[0] + cnIntLast + cnInteger;
+    return chineseStr;
+  }
+  // 转换为字符串
+  money = money.toString();
+  if (money.indexOf('.') === -1) {
+    integerNum = money;
+    decimalNum = '';
+  } else {
+    parts = money.split('.');
+    integerNum = parts[0];
+    decimalNum = parts[1].substr(0, 4);
+  }
+  // 获取整型部分转换
+  if (parseInt(integerNum, 10) > 0) {
+    let zeroCount = 0;
+    const IntLen = integerNum.length;
+    for (let i = 0; i < IntLen; i++) {
+      const n = integerNum.substr(i, 1);
+      const p = IntLen -i - 1;
+      const q = p / 4;
+      const m = p % 4;
+      if (n === '0') {
+        zeroCount++;
+      } else {
+        if (zeroCount > 0) {
+          chineseStr += cnNums[0];
+        }
+        // 归零
+        zeroCount = 0;
+        chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
+      }
+      if (m === 0 && zeroCount < 4) {
+        chineseStr += cnIntUnits[q];
+      }
+    }
+    chineseStr += cnIntUnits[q];
+  }
+  // 小数部分
+  if (decimalNum !== '') {
+    const decLen = decimalNum.length;
+    for (let i = 0; i < decLen; i++) {
+      const n = decimalNum.substr(i, 1);
+      if (n !== '0') {
+        chineseStr += cnNums[Number(n)] + cnDecUnits[i]; 
+      }
+    }
+  }
+  if (chineseStr === '') {
+    chineseStr += cnNums[0] + cnIntLast + cnInteger; 
+  } else if(decimalNum === '') {
+    chineseStr += cnInteger;
+  }
+  return chineseStr;
 }
